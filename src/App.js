09d04e974ad19480ebe362b3c745b99ac607087c
Jason Schmitt <schmitty890@gmail.com>
2019-07-16 18:17:21 -0400

    adding actions to update redux store for incrememtning and decrementing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
+
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      {isLogged ? <h3>Info we can't see unless we're logged in</h3> : ''}
+      
     </div>
   );
 }
