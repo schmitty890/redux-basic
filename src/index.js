e05f2ede5420ed662fb082ddcb7c0a0ece86898b
Jason Schmitt <schmitty890@gmail.com>
2019-07-16 18:04:17 -0400

    adding all reducers together and into store in main index.js file

--- src/index.js
+++ src/index.js
@@ -4,41 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
+import allReducers from './reducers';
 
-// STORE - Globalized state
+let store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-// ACTION INCREMENT
-const increment = () => {
-    return {
-        type: 'INCREMENT'
-    }
-}
-
-const decrement = () => {
-    return {
-        type: 'DECREMENT'
-    }
-}
-
-// REDUCER
-const counter = (state = 0, action) => {
-    switch(action.type) {
-        case 'INCREMENT': 
-            return state + 1;
-        case 'DECREMENT': 
-            return state - 1;
-    }
-}
-let store = createStore(counter);
-
-// Display it in the console
-store.subscribe(() => console.log(store.getState()));
-
-// DISPATCH
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(decrement());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
