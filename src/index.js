3b800d145177153e452b6174fbded570b17cf9c2
Jason Schmitt <schmitty890@gmail.com>
2019-07-16 17:47:28 -0400

    adding redux and increment and decrement to add state to store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+
+// STORE - Globalized state
+
+// ACTION INCREMENT
+const increment = () => {
+    return {
+        type: 'INCREMENT'
+    }
+}
+
+const decrement = () => {
+    return {
+        type: 'DECREMENT'
+    }
+}
+
+// REDUCER
+const counter = (state = 0, action) => {
+    switch(action.type) {
+        case 'INCREMENT': 
+            return state + 1;
+        case 'DECREMENT': 
+            return state - 1;
+    }
+}
+let store = createStore(counter);
+
+// Display it in the console
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATCH
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
